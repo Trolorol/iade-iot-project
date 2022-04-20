fff3f6cb74cddedb377d78d3f97367879f6a4326
Frederico Costa Ramalho <Fredcostar@gmail.com>
2022-04-20 16:33:41 +0100

    Publish e subscribe mqtt

--- services/mqtt-service.js
+++ services/mqtt-service.js
@@ -5,3 +5,32 @@ function publishWithMqtt() {
   // que serve para comunicação entre aquele device daquele user
   // É necessario fazer um puiblish com o nome do user_id/device_id para o pedido ir parar ao device que está a escutar aquele lista especifica
 }
+
+
+
+mosquitto_sub -h ;
+mqtt.eclipseprojects.io -t;
+ "ProjetoIoT/iade"
+
+const topic = '/sensor/temperatura'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([sensor/temperatura], () => {
+    console.log(`Subscribe to topic '${sensor/temperatura}'`)   //Subscribe ao tópico sensor/temperatura
+  })
+})
+
+client.on('message', (sensor/temperatura, payload) => {
+  console.log('Received Message:', sensor/temperatura, payload.toString()) //converter mensagem buffer type para string
+})
+
+client.on('connect', () => {
+  client.publish(sensor/temperatura, 'nodejs mqtt teste', { qos: 0, retain: false }, (error) => {  //publish no tópico sensor/temperatura
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+
+
+
